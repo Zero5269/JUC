6c1f1427205334860b563cb2752a1a4d74328941
Zero5269 <945246041@qq.com>
2021-11-01 18:09:33 +0800

    update

--- src/main/java/com/zero/Tets.java
+++ src/main/java/com/zero/Tets.java
@@ -1,10 +1,16 @@
 package com.zero;
 
+import sun.misc.Unsafe;
+
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Tets {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException {
         ConcurrentHashMap map = new ConcurrentHashMap();
         map.put("1", "msb");
+        Unsafe unsafe = Unsafe.getUnsafe();
+        System.out.println(AtomicInteger.class.getDeclaredField("value"));
+        System.out.println(unsafe.objectFieldOffset(AtomicInteger.class.getDeclaredField("value")));
     }
 }
--- src/main/java/com/zero/juc/c_018_01_Unsafe/HelloUnsafe.java
+++ src/main/java/com/zero/juc/c_018_01_Unsafe/HelloUnsafe.java
@@ -4,16 +4,23 @@ package com.zero.juc.c_018_01_Unsafe;
 
 import sun.misc.Unsafe;
 
+import java.lang.reflect.Field;
+
 public class HelloUnsafe {
     static class M {
-        private M() {}
+        private M() {
+        }
 
-        int i =0;
+        int i = 7;
     }
 
-   public static void main(String[] args) throws InstantiationException {
-        Unsafe unsafe = Unsafe.getUnsafe();
-        M m = (M)unsafe.allocateInstance(M.class);
+    public static void main(String[] args) throws Exception {
+//        Unsafe unsafe = Unsafe.getUnsafe();
+        Field f = Unsafe.class.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+        Unsafe unsafe = (Unsafe) f.get(null);
+        M m = (M) unsafe.allocateInstance(M.class);
+        System.out.println(m.i);
         m.i = 9;
         System.out.println(m.i);
     }
